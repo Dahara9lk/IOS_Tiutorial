0d4f1a7fda6a19e8cfc85cc67814a15b1b3d8a54
Dahara9lk <dahararathnayaka08@gmail.com>
2026-06-13 12:19:26 +0530

    initial commit

---
+++ IOS_Tutorial/IOS_Tutorial/IOS_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_TutorialApp.swift
+//  IOS_Tutorial
+//
+//  Created by Student4 on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct IOS_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
