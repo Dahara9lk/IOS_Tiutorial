cd9a89faba4f43bed32fbf928e4ec5a30583cb15
Dahara9lk <dahararathnayaka08@gmail.com>
2026-07-10 19:17:11 +0530

    Fixed all compilation errors and updated UI components.

--- IOS_Tutorial/IOS_Tutorial/Services/LocationService.swift
+++ IOS_Tutorial/IOS_Tutorial/Services/LocationService.swift
@@ -5,91 +5,71 @@
 //  Created by student8 on 2026-07-09.
 //
 
-import CoreLocation
 import Combine
+import CoreLocation
+import Foundation
 
-class LocationService: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private let manager = CLLocationManager()
-    
+final class LocationService: NSObject, ObservableObject {
     @Published var currentLocation: CLLocation?
-    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
     @Published var lastError: Error?
-    @Published var isUpdating = false
-    
+    @Published private(set) var authorizationStatus: CLAuthorizationStatus
+
+    private let locationManager = CLLocationManager()
+
     override init() {
+        authorizationStatus = locationManager.authorizationStatus
         super.init()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.distanceFilter = 10 // Update every 10 meters
-        manager.allowsBackgroundLocationUpdates = false
-        manager.pausesLocationUpdatesAutomatically = true
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
-    
+
     func requestPermission() {
-        manager.requestWhenInUseAuthorization()
-    }
-    
-    func startUpdating() {
-        guard !isUpdating else { return }
-        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
-            manager.startUpdatingLocation()
-            isUpdating = true
-            print("📍 Started updating location")
-        }
-    }
-    
-    func stopUpdating() {
-        manager.stopUpdatingLocation()
-        isUpdating = false
-        print("📍 Stopped updating location")
-    }
-    
-    // MARK: - CLLocationManagerDelegate
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
-        
-        switch authorizationStatus {
+        switch locationManager.authorizationStatus {
         case .notDetermined:
-            print("📍 Location: Not determined")
-        case .restricted:
-            print("📍 Location: Restricted")
-        case .denied:
-            print("📍 Location: Denied - Please enable in Settings")
-        case .authorizedWhenInUse, .authorizedAlways:
-            print("📍 Location: Authorized")
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
             startUpdating()
+        case .denied, .restricted:
+            authorizationStatus = locationManager.authorizationStatus
         @unknown default:
-            break
+            authorizationStatus = locationManager.authorizationStatus
         }
     }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        
-        DispatchQueue.main.async {
-            self.currentLocation = location
+
+    func startUpdating() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case .denied, .restricted:
+            authorizationStatus = locationManager.authorizationStatus
+        @unknown default:
+            authorizationStatus = locationManager.authorizationStatus
         }
-        
-        print("📍 Location updated: \(location.coordinate.latitude), \(location.coordinate.longitude)")
-        print("📍 Accuracy: \(location.horizontalAccuracy)m")
     }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        DispatchQueue.main.async {
-            self.lastError = error
-        }
-        print("❌ Location error: \(error.localizedDescription)")
-        
-        if let clError = error as? CLError {
-            switch clError.code {
-            case .denied:
-                print("📍 Location services denied")
-            case .network:
-                print("📍 Network error fetching location")
-            default:
-                break
-            }
-        }
+
+    func stopUpdating() {
+        locationManager.stopUpdatingLocation()
     }
 }
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+
+        if manager.authorizationStatus == .authorizedAlways || manager.authorizationStatus == .authorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        lastError = error
+    }
+}
+
